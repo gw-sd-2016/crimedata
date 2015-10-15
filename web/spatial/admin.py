ce560b6f7d96913e9aba85f3a025e797f061be44
Ben Carleton <ben@bencarleton.com>
2015-10-14 20:16:02 -0400

    * Add Incident Narrative * Fixup incident admin page

--- web/spatial/admin.py
+++ web/spatial/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis import admin as gis_admin
 from spatial.models import CrimeType, Incident
 
-# Register your models here.
+# (Very simple) default associations of the models to the builtin Admin Site
+# This will allow for basic validation during model design
+
+
+class IncidentAdminOverride(gis_admin.OSMGeoAdmin):
+    readonly_fields = ('lat', 'lon')
+
+
 admin.site.register(CrimeType)
-admin.site.register(Incident)
+admin.site.register(Incident, IncidentAdminOverride)
--- web/spatial/models.py
+++ web/spatial/models.py
@@ -6,6 +6,10 @@ class CrimeType(models.Model):
     friendly_name = models.CharField(null=False, blank=False, max_length=240)
     severity = models.IntegerField(default=5, null=False, blank=False)
 
+    def __unicode__(self):
+        return self.friendly_name
+
+
 
 class Incident(gis_models.Model):
     incident_type = gis_models.ForeignKey(CrimeType, null=True, on_delete=gis_models.SET_NULL)
@@ -13,3 +17,10 @@ class Incident(gis_models.Model):
     point = gis_models.PointField()
     lat = gis_models.DecimalField(max_digits=10, decimal_places=6)
     lon = gis_models.DecimalField(max_digits=10, decimal_places=6)
+    narrative = gis_models.CharField(null=True, blank=True, max_length=1024)
+
+    def save(self, *args, **kwargs):
+        self.lat = self.point.y
+        self.lon = self.point.x
+
+        super(Incident, self).save(*args, **kwargs)
