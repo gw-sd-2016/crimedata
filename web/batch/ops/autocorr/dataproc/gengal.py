5eb37ea363c3e8eb98e75baff9b5df3a039f5138
Ben Carleton <ben@bencarleton.com>
2016-01-18 15:12:01 -0500

    Be quiet

--- web/batch/ops/autocorr/dataproc/gengal.py
+++ web/batch/ops/autocorr/dataproc/gengal.py
@@ -10,14 +10,14 @@ def generate_weights(sdiv_set=None):
     if sdiv_set is None:
         sdiv_set = Subdivision.objects.filter(src_file_index__lt=100)
 
-    print("Got %d SDivs" % len(sdiv_set))
+    # print("Got %d SDivs" % len(sdiv_set))
 
     neighbor_set = dict()
     for subdiv in sdiv_set: # type: Subdivision
         subdiv_neighbors = Subdivision.objects.filter(polygon__touches=subdiv.polygon)
         neighbor_set[subdiv.src_file_index] = [x.src_file_index for x in subdiv_neighbors]
 
-    print(neighbor_set)
+    # print(neighbor_set)
     file_path = "%sgengal_weights_%s.bin" % (settings.TMP_BASE, uuid.uuid4())
     with open(file_path, "wb") as outfile:
         pickle.dump(neighbor_set, outfile, PICKLE_VERSION)
