f1afea767187f0ad80bb3ac1123f8a278b95e7fa
Ben Carleton <ben@bencarleton.com>
2016-03-06 21:11:47 -0500

    Interval-based analysis view now works in terms of getting and adding the data to the map. Now need to add the "play/pause"-style control so the layers don't stack.

--- web/spatial/views.py
+++ web/spatial/views.py
@@ -6,6 +6,7 @@ from django.contrib.gis.measure import D
 from djgeojson.views import GeoJSONLayerView
 
 import datetime
+import json
 from spatial.models import CrimeType, Subdivision
 from batch.ops.autocorr import stl_process
 
@@ -32,6 +33,7 @@ def mapview2(request):
     ctid_select = request.POST.get("ctid")
     sset = None
     interval_indivisible = False
+    active_subdivisions = None
 
     if request.POST:
         bound_nw_lat = float(request.POST.get('nw_lat'))
@@ -59,56 +61,73 @@ def mapview2(request):
             polygon__within=bounding_polygon
         )
 
+        interval = None
+        if ctid_select is not None:
+            # Process spatial autocorrelation based on supplied arguments
+            interval = request.POST.get("interval")
+            date_groups = []
+            if interval is not None and interval != "":
+                '''
+                If the user did not supply an interval, this all is skipped and the
+                supplied params (start/end, inc. type) are passed directly to
+                stl_process (the spatial autocorrelation processor).
 
-    if ctid_select is not None:
-        # Process spatial autocorrelation based on supplied arguments
-        interval = request.POST.get("interval")
-        date_groups = []
-        if interval is not None and interval != "":
-            '''
-            If the user did not supply an interval, this all is skipped and the
-            supplied params (start/end, inc. type) are passed directly to
-            stl_process (the spatial autocorrelation processor).
+                If the user DID supply an interval, that means that we want to split the
+                supplied date range by the interval and run the autocorrelation processor
+                on each interval. The results from each interval will then be displayed
+                sequentially on the map.
+                    Example:
+                        Start date: 2015-05-19
+                        End date: 2015-05-25
+                        Interval: 1
+                        Crime type ID: 1
 
-            If the user DID supply an interval, that means that we want to split the
-            supplied date range by the interval and run the autocorrelation processor
-            on each interval. The results from each interval will then be displayed
-            sequentially on the map.
-                Example:
-                    Start date: 2015-05-19
-                    End date: 2015-05-25
-                    Interval: 1
-                    Crime type ID: 1
+                        This input would run the analysis for incidents of CTID=1
+                        for each day (interval=1) in the range, and then pass the results
+                        for each day into their own layer on the map. The map UI will then
+                        loop through each layer showing the change over time.
+                '''
+                try:
+                    interval = int(request.POST.get("interval"))
+                except:
+                    raise ValueError("Unable to cast interval to int")
 
-                    This input would run the analysis for incidents of CTID=1
-                    for each day (interval=1) in the range, and then pass the results
-                    for each day into their own layer on the map. The map UI will then
-                    loop through each layer showing the change over time.
-            '''
-            try:
-                interval = int(request.POST.get("interval"))
-            except:
-                raise ValueError("Unable to cast interval to int")
+                dt_start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+                dt_end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+                date_range_delta = (dt_end_date - dt_start_date)      # type: datetime.timedelta
+                date_range_length = date_range_delta.days
 
-            dt_start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
-            dt_end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
-            date_range_delta = (dt_end_date - dt_start_date)      # type: datetime.timedelta
-            date_range_length = date_range_delta.days
+                if date_range_length % interval != 0:
+                    interval_indivisible = True
 
-            if date_range_length % interval != 0:
-                interval_indivisible = True
+                active_subdivisions = []
+                current_date = dt_start_date
+                while True:
+                    current_end_date = current_date + datetime.timedelta(days=interval)
+                    if current_end_date > dt_end_date:
+                        current_end_date = dt_end_date
+                        print("CD = %s CED = %s DED = %s" % (current_date, current_end_date, dt_end_date))
 
+                    print("DR => %s - %s" % (current_date, current_end_date))
+                    current_autocorr_result = stl_process(sset, current_date, current_end_date, ctid_select)
+                    if len(current_autocorr_result) == len(sset):
+                        current_autocorr_result = []
+                    print("\tSAC res len = %d" % len(current_autocorr_result))
+                    print("\tSAC res mem = %s" % current_autocorr_result)
 
-
-        active_subdivisions = stl_process(sset, start_date, end_date, ctid_select)
-        #active_subdivisions = ()
-    else:
-        active_subdivisions = ()
-    # print(active_subdivisions)
-    # print(start_date, end_date, ctid_select)
-    # print(request.POST)
+                    active_subdivisions.append(current_autocorr_result)
+                    if current_end_date >= dt_end_date:
+                        break
+                    current_date = current_end_date
+            else:
+                # An interval was not selected so just run the analysis against the total date range
+                active_subdivisions = [stl_process(sset, start_date, end_date, ctid_select)]
+        # print(active_subdivisions)
+        # print(start_date, end_date, ctid_select)
+        # print(request.POST)
 
     subdivisons = Subdivision.objects.all() #.filter(display_name__icontains="County")
+    active_subdivisions = json.dumps(active_subdivisions)
 
     return render_to_response("mapview2.html", locals(), context_instance=RequestContext(request))
 
