aa159b95b8fb6d93f365042169ad235b732a6994
Ben Carleton <ben@bencarleton.com>
2016-05-12 08:16:29 -0400

    Add the rest of the files

---
+++ web/web/local_settings.py
@@ -0,0 +1,11 @@
+GOOGLE_GEOCODE = {
+    # Google Project Name: SD 2016
+    # Google Project ID: axial-crane-112517
+    # Google API key: "bgc-cssd-001 - Django"
+    # The source address for requests against this key is set to 128.164.243.0/32
+
+    'API_KEY': "AIzaSyAl_5DWXXcasFqbpOlPM8EavTSZA07XVBY",
+    'API_URL': "https://maps.googleapis.com/maps/api/geocode/json",
+}
+
+GWUPD_DATA_CSV_PATH = "/home/ben/gwu_cdata4.csv"
